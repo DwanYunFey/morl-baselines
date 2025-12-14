0f46f4c5b234010266ac0ebeb4b52f82d9d723aa
DwanYunFey <duan-yun-fei@qq.com>
2025-12-14 12:26:52 +0800

    fix:依赖安装

--- build.sh
+++ build.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
-# conda环境
-echo "执行conda create -n morl python=3.10"
-conda create -n morl python=3.10
-
-echo "执行conda activate morl"
-conda activate morl
 
 # Darwin环境下不能跑gpi和ols算法
-# 安装依赖
 OS="$(uname -s)"
 if [ "$OS" = "Darwin" ]; then
-    echo "执行: pip install \".[testing]\""
-    pip install ".[testing]"
+    conda env create -f environment.darwin.yml
 elif [ "$OS" = "Linux" ]; then
-    echo "执行: pip install -e \".[testing, all]\""
-    pip install -e ".[testing, all]"
+    conda env create -f environment.linux.yml
 fi
-# 检查安装是否成功
-if [ $? -eq 0 ]; then
-    echo "依赖安装成功！"
-else
-    echo "依赖安装失败！"
-fi
-python -c "import morl_baselines; print('✅ morl-baselines 导入成功')"
+
+python -c "import morl_baselines; print('✅ morl-baselines 依赖安装成功')"
 
 wandb login --relogin
 # wandb API Key:
